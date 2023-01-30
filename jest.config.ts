62c212c4de9563547eedd7a1282d1337aa1ee294
zhuba-Ahhh <3477826311@qq.com>
2023-01-30 22:13:57 +0800

    修复: :bug: 修复jest测试 添加忽略目录
    jest.config.ts配置modulePathIgnorePatterns子段

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,10 @@ import type { Config } from "jest";
 const config: Config = {
   collectCoverageFrom: ["**/*test.{js,jsx,ts,tsx}", "!**/node_modules/**"],
   // 忽略名单
-  modulePathIgnorePatterns: ["<rootDir>/src/06-jest_demo"],
+  modulePathIgnorePatterns: [
+    "<rootDir>/src/06-jest_demo",
+    "<rootDir>/src/07-test_demo",
+  ],
 };
 
 export default config;
